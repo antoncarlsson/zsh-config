bbcfb420030af82eb971e501b465f81ca7471ee4
Anton Carlsson <anton.carlsson@valtech.com>
2019-09-13 02:56:03 +0200

    Generate the LS_COLORS from the snazzy theme.

--- lib/ls_color.zsh
+++ lib/ls_color.zsh
@@ -4,6 +4,4 @@
 
 #-----------------------------------------------------
 # Add colors to ls command
-if [ -f "/usr/local/bin/gdircolors"  ] || [ -f "/usr/bin/gdircolors"  ]; then
-	eval $( gdircolors -b ${ZSHCONFIG}/lib/dircolors-custom )
-fi
+export LS_COLORS="$(vivid generate snazzy)"
